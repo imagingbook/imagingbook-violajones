7c2e62a060d1757251e13cb5a3db059e9b1cc644
imagingbook <wilhelm.burger@fh-hagenberg.at>
2020-11-02 11:08:36 +0100

    JavaDoc rebuild (with frames)

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"face_plugins"},{"l":"face_plugins_tests"},{"l":"imagingbook.violajones.lib"},{"l":"imagingbook.violajones.lib.util"},{"l":"imagingbook.violajones.resources"},{"l":"imagingbook.violajones.resources.xml"}]
