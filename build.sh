5385f1793f11dba2c873d72858b62435e2fe3bfa
imagingbook <mail.wilhelm.burger@gmail.com>
2022-12-05 18:33:25 +0100

    build.sh updated

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ mvn install -Dmaven.test.skip=true
 
 rm -rf javadoc/*
 mvn javadoc:aggregate -Dimagingbook.skipjavadoc=false
+mvn javadoc:aggregate-jar -Dimagingbook.skipjavadoc=false
+
+# mvn deploy -Dmaven.test.skip=true -Dimagingbook.skipjavadoc=false -Dimagingbook.gpgSkip=false
+mvn deploy -Dmaven.test.skip=true -Dimagingbook.skipjavadoc=true -Dimagingbook.gpgSkip=true
 
 echo ""
 read -rsp $'Done. Press any key to quit...\n' -n1 key
